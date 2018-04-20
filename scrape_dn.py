ea3ae245a4b364206562295ca5fd92bc482e8de2
Oscar Holm <oscho707@student.liu.se>
2018-04-20 16:56:50 +0200

    .

--- scrape_dn.py
+++ scrape_dn.py
@@ -1,36 +1,93 @@
-import os, requests
-from HTMLParser import HTMLParser
+import sys, requests
+from html.parser import HTMLParser
+
+interesting = {
+    'data-page-title':'',
+    'data-article-title':'',
+    'article-image':'',
+    'data-authors':'',
+    'data-article-description':'',
+    'data-article-image':'',
+    'data-article-publish-date':'',
+    'data-article-publish-time':'',
+    'data-is-premium':'',
+    'data-access-locked':'',
+    'article__body':'',
+    'article__lead':'',
+    'article__body-content':'',
+}
+my_tags = ['p','h1']
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
-        self.parse = False
+        self.found_article = False
+        self.found_body = False
+        self.parse_content = False
+        self.article = {'body':[],'description':''}
+        HTMLParser.__init__(self)
+        
     
     def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag
-        if tag is 'div' and 'js-article' in attrs:
-            parse = True
+        if not self.found_article and tag == 'div':
+            #print("Possible start of article parsing on tag: ", tag)
+            for attr in attrs:
+                if self.found_article:
+                    if attr[0] in interesting:
+                        self.article[attr[0]] = attr[1]
+                elif attr[1] == 'js-article':
+                    #print("Found start of parsing tag via attr: ", attr)
+                    self.found_article = True
+        elif not self.found_body and tag == 'div':
+            #print("Possible start of body parsing on tag: ", tag)
+            for attr in attrs:
+                if attr[1] == 'article__body':
+                    #print("Found start of body parsing tag via attr: ", attr)
+                    self.found_body = True
+            
+        elif self.found_body and tag == 'p':
+            self.parse_content = True            
+            
+            
 
     def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
-        if parse:
-            parse = False
+        #print("Encountered an end tag : ", tag)
+        if tag == 'p':
+            parse_content = False
+        elif tag == 'main':
+            self.found_body = False
 
     def handle_data(self, data):
-        #print "Encountered some data  :", data
-        
+        if self.found_body and self.parse_content:
+            self.article['body'].append(data)
+            #print("Want to parse this data: ", data)
 
-if len(os.argv) is not 2:
-    print('Usage: python3 scrape_dn.py url')
-    return 1
+    def write_article(self):
+        print(self.article)
+        page_title = self.article['data-page-title']
+        article_title = self.article['data-article-title']
+        img_url = self.article['article_image'] if 'article-image' in self.article else ''
+        filename = page_title.replace(' ', '_').lower() + '.html'
+        with open(filename, 'w') as f:
+            f.write('<html><head><title>{}</title></head>'.format(page_title))
+            f.write('<body><h1>{}</h1>'.format(article_title))
+            if len(img_url) > 0:
+                f.write('<img src="{}">'.format(img_url))
+            for item in self.article['body']:
+                f.write('<p>{}</p>'.format(item))
+            f.write('</body></html>')
+            
 
-r = requests.get(os.argv[1])
-if r.status_code is 200:
-    parser = MyHTMLParser()
-    parser.feed(r.text)
-
-    parser.close()
-    
+if len(sys.argv) < 2:
+    print('Usage: python3 scrape_dn.py urls')
 else:
-    print('Request failed, status code %', r.status_code)
-    return 1
-        
+    urls = sys.argv[1:]
+    for url in urls:
+        r = requests.get(url)
+        if r.status_code is 200:
+            parser = MyHTMLParser()
+            parser.feed(r.text)
+            parser.write_article()
+            parser.close()
+        else:
+            print('Request failed on url %, status code %', url, r.status_code)
+
